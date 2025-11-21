160e47612e8905f4569cf33716ea73d44ff4805f
Abdelrahamane.reguig <abdelrahmane.reguig@etu.univ-amu.fr>
2025-11-21 10:50:48 +0100

    Update vote-majoritaire2tours.cpp

--- vote-majoritaire2tours.cpp
+++ vote-majoritaire2tours.cpp
@@ -10,21 +10,38 @@ int main()
     cout << "Nombre de votants : ";
     cin >> nbvotant;
     vector<int> resultatcandidat(nbcandidat, 0);
+    int vote;
+    int voteblanc = 0;
+    int maxvote1 = 0;
+    int maxvote2 = 0;
     for (int i = 0; i < nbvotant; i++)
     {
-        int vote;
-        int voteblanc = 0;
-        cout << "Vote du n°" << (i + 1);
+        cout << "Vote du n°" << (i + 1) << ": ";
         cin >> vote;
         if (vote >= 1 && vote <= nbcandidat)
         {
             resultatcandidat[vote - 1] += 1;
-            cout << "Vote pour le candidat n°" << vote << endl;
+            cout << "Vote pour le candidat n° : " << vote  << endl;
         }
         else
         {
             cout << "Vote blanc" << endl;
             voteblanc += 1;
         }
+        if (maxvote2 < resultatcandidat[vote - 1])
+        {
+            maxvote2 = resultatcandidat[vote - 1];
+        }
+        if (maxvote1<maxvote2)
+        {
+            maxvote1 = maxvote2;
+        }
     }
+    for (int i = 0; i < nbcandidat; i++)
+    {
+        cout << "Resultat pour le candididat n°" << i + 1 << ": " << resultatcandidat [i] << endl;
+    }
+    cout << "Nombre de vote blanc : " << voteblanc << endl;
+    cout << maxvote2 << endl;
+    cout << maxvote1 << endl;
 }
