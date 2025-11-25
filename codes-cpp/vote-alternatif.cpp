6e2fb98df245c1f3ec40c712cd78bcea9639f9d8
Alexandre-SBEGHEN <alexandre.sbeghen-magnin@etu.univ-amu.fr>
2025-11-25 15:35:20 +0100

    ajout vote alt

--- codes-cpp/vote-alternatif.cpp
+++ codes-cpp/vote-alternatif.cpp
@@ -1,1 +1,59 @@
-//code à insérer ici
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string litUneString (){
+    string uneChaine;
+    while (true){
+        getline (cin, uneChaine);
+        if ((!cin) || (uneChaine.substr(0,2) != "//")) break;
+    }
+    return uneChaine;
+}
+
+int litUnEntier (){
+    string uneChaine;
+    while (true){
+        getline (cin, uneChaine);
+        if ((!cin) || (uneChaine.substr(0,2) != "//")) break;
+    }
+    return stoi(uneChaine);
+}
+
+struct participant {
+    string nom;
+    string prenom;
+    int glacePref;
+};
+
+int main() {
+    size_t nombre_candidats = 4;
+
+    //Lire les participants et les stocker dans une liste
+    vector<string> liste_candidats;
+    for (size_t i = 0; i < nombre_candidats; ++i) {
+        liste_candidats.push_back(litUneString());
+    }
+
+    //Lire les voies
+    vector<participant> liste_participants;
+    vector<size_t> liste_votes(nombre_candidats, 0);
+
+    while (cin) {
+        string nom = litUneString();
+        string prenom = litUneString();
+        size_t vote = litUnEntier();
+
+        participant part;
+        part.nom = nom;
+        part.prenom = prenom;
+        part.glacePref = vote;
+
+        liste_participants.push_back(part);
+        liste_votes[vote] += 1;
+    }
+
+    for (size_t i: liste_votes) cout << i << endl;
+
+    return 0;
+}
